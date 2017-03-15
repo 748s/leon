b67338b62bcb47bf3cdccb3abb8f7854c19cf3ef
Nick Wakeman <nick.wakeman@gmail.com>
2017-03-15 11:57:59 -0400

    Fix Form/Element/Document, add 'container' block to base.html.twig

--- src/Form/Element/Document.php
+++ src/Form/Element/Document.php
@@ -10,12 +10,8 @@ class Document extends File
 {
     protected $type = 'document';
     protected $fileTypes = [
-        'doc' => 'application/msword',
-        'docm' => 'application/vnd.ms-word.document.macroenabled.12',
-        'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'dot' => 'application/msword',
-        'dotm' => 'application/vnd.ms-word.template.macroenabled.12',
-        'dotx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
-        'pdf' => 'application/pdf',
+        DOC::class,
+        DOCX::class,
+        PDF::class,
     ];
 }
